52b0c23a5ba324f82f15c74b7dfdde5eba5890ce
SreeharshaReddy <harshapriya2353@gmail.com>
2016-04-11 10:38:42 -0700

    commit this

---
+++ gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: '**/gruntfile.js',
+        dest: 'build/',
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+
+};
